b2f249c4ce0e5b6fa682e7ab2a5072eaac19ac59
ToshiBiswas <92240916+ToshiBiswas@users.noreply.github.com>
2025-02-05 14:37:18 -0700

    fixed bugs

--- scripts/Quiz.js
+++ scripts/Quiz.js
@@ -1,77 +1,83 @@
 /**
 * FUNCIONALITY: creates the quiz content questions,and stores the score.
  */
-    class Quiz{
-
+    /**
+ * FUNCIONALITY: creates the quiz content questions,and stores the score.
+ */
+class Quiz {
     #questions;
     #score;
     #order;
     #questionsNum;
     #checkValidation = false;
+    /*  
+          FUNCTIONALITY: Initializes all the quiz questions that will be asked for the quiz session.
+  
+          Parameters: number of questions to initialize
+  
+          Returns: None
+      */
+    constructor(questions) {
+      this.#score = 0;
+      this.#order = 1;
+      this.#questionsNum = questions;
+      this.#questions = [];
+      // for (let q = 0; q < questions; q++) {
+      //   var temp = new Question();
+      //   this.initQuestion(temp);
+      // }
+    }
+  
+    //   async initQuestion(temp) {
+    //     console.log(await temp.intializeQuestion());
+    //     return true;
+    //   }
     /* 
-        FUNCTIONALITY: Initializes all the quiz questions that will be asked for the quiz session.
-
-        Parameters: number of questions to initialize
-
-        Returns: None
-    */
-    constructor(questions){        
-        this.#score = 0;
-        this.#order = 1;
-        this.#questionsNum =questions;
-        this.#questions = [];
-        for(var q=0; q<questions;q++){
-            var temp = new Question();
-            temp.intializeQuestion();
-            this.#questions.push(temp);
-        }
-
+          FUNCTIONALITY: Initializes all the quiz questions that will be asked for the quiz session.
+  
+          Parameters: None
+  
+          Returns: Question variable
+      */
+    async getNextQuestion() {
+      // if (this.#order > this.#questionsNum) {
+      //   throw new exception("function order has been maxed out");
+      // }
+      // this.#checkValidation = true;
+      // console.log(this.#questions[this.#order - 1]);
+  
+      // return this.#questions[this.#order - 1];
+      let temp = new Question();
+      await temp.intializeQuestion();
+      return temp;
     }
     /* 
-        FUNCTIONALITY: Initializes all the quiz questions that will be asked for the quiz session.
-
-        Parameters: None
-
-        Returns: Question variable
-    */
-    getNextQuestion() {
-        if(this.#order > this.#questionsNum){
-            throw new exception("function order has been maxed out")
-        }
-        this.#checkValidation =true;
-        console.log(this.#questions[this.#order-1]);
-
-        return this.#questions[this.#order-1];
+          FUNCTIONALITY: Initializes all the quiz questions that will be asked for the quiz session.
+  
+          Parameters: None
+  
+          Returns: Question variable
+      */
+    getScore() {
+      return this.#score;
     }
-    /* 
-        FUNCTIONALITY: Initializes all the quiz questions that will be asked for the quiz session.
-
-        Parameters: None
-
-        Returns: Question variable
-    */
-    getScore(){
-        return this.#score;
+  
+    validateQuestion(answer) {
+      if (this.#checkValidation) {
+        return;
+      }
+      if (this.#questions.at(this.#order++ - 1).validateQuestion(answer)) {
+        this.#score++;
+        this.#checkValidation = false;
+        return true;
+      } else {
+        this.#score++;
+        this.#checkValidation = false;
+        this.#order++;
+        return answer;
+      }
     }
-
-    validateQuestion(answer){
-        if(this.#checkValidation){
-            return;
-        }
-        if(this.#questions.at(this.#order++-1).validateQuestion(answer)){
-            this.#score++;
-            this.#checkValidation = false;
-            return true;
-        }else{
-            this.#score++;
-            this.#checkValidation = false;
-            this.#order++;
-            return answer;
-        }
+    getOrder() {
+      return this.#order;
     }
-    getOrder(){
-        return this.#order;
-    }
-
-     
-}
+  }
--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,59 @@
-
 let quiz;
 
 let quizActive = false;
-let quizConcluded= false;
+let quizConcluded = false;
 let questionActive = false;
 let questionNow;
 let score = 0;
+let order = 0;
 const form = document.getElementById("start");
-form.addEventListener('click', function (event) {
-    score = 0;
-    quizActive = true;
-    quiz = new Quiz(10);
-    questionActive = true;
-    setNextQuestion();
-    
+form.addEventListener("click", function (event) {
+  score = 0;
+  quizActive = true;
+  quiz = new Quiz(10);
+  questionActive = true;
+  setNextQuestion();
 });
-function setNextQuestion(){
-    if(!(quiz.getOrder() >=10)){
-        console.log()
-        questionNow = quiz.getNextQuestion();
-        var video = document.getElementById("video");
-        video.setAttribute("src",questionNow.getVideo());
-        document.getElementsById("first_Option").innerText = questionNow.getOptions().at(0);
-        document.getElementsById("second_Option").innerText = questionNow.getOptions().at(1);
-        video.setInnerText;
-    }else{
-        quizActive = false;
-    }
-    
-
+async function setNextQuestion() {
+  if (!(quiz.getOrder() >= 10)) {
+    console.log();
+    questionNow = await quiz.getNextQuestion();
+    var video = document.querySelector(".section__video");
+    console.log(questionNow);
+    console.log(questionNow.getVideo());
+    video.setAttribute("src", questionNow.getVideo());
+    document.getElementById("first_Option").innerText = questionNow
+      .getOptions()
+      .at(0);
+    document.getElementById("second_Option").innerText = questionNow
+      .getOptions()
+      .at(1);
+    video.setInnerText;
+  } else {
+    quizActive = false;
+  }
 }
 const form2 = document.getElementById("first_Option");
-form2.addEventListener('click', function (event) {
-    event.preventDefault();
-    if(quizActive){
-        var answer = quiz.validateQuestion(questionNow.getOptions().at(0));
-        if(answer){
-            let scoreVal = document.getElementsByClassName("section__score");
-            scoreVal.innerText = "Score: " + String(++score);
-        }
-        setNextQuestion();
+form2.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (quizActive) {
+    if (questionNow.validateAnswer(questionNow.getOptions().at(0))) {
+      let scoreVal = document.getElementsByClassName("section__score");
+      scoreVal.innerText = "Score: " + String(++score);
     }
+    order++;
+    setNextQuestion();
+  }
 });
 const form3 = document.getElementById("second_Option");
-form2.addEventListener('click', function (event) {
+form3.addEventListener("click", function (event) {
     event.preventDefault();
-    if(quizActive){
-        var answer = quiz.validateQuestion(questionNow.getOptions().at(1));
-        if(answer){
-            let scoreVal = document.getElementsByClassName("section__score");
-            scoreVal.innerText = "Score: " + String(++score);
-        }
-        setNextQuestion();
+    if (quizActive) {
+      if (questionNow.validateAnswer(questionNow.getOptions().at(1))) {
+        let scoreVal = document.getElementsByClassName("section__score");
+        scoreVal.innerText = "Score: " + String(++score);
+      }
+      order++;
+      setNextQuestion();
     }
-});
+  });
